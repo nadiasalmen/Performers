9bfe9d30653f8d09f8800058d3f89402a4d09940
Nadia Salmen <nadiasalmen@gmail.com>
2020-12-25 22:54:58 -0300

    Us page - v1 - wip: background image, popups

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :pindex ]
+  skip_before_action :authenticate_user!, only: [ :home, :us ]
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'pindex', to: 'pages#pindex'
+  get 'us', to: 'pages#us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
