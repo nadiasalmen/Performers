9ca269627cd9349cbafe4b1a2e9e47a60c69306d
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-09 10:29:34 -0300

    Add Performances model

--- app/models/performance_category.rb
+++ app/models/performance_category.rb
@@ -1,5 +1,6 @@
 class PerformanceCategory < ApplicationRecord
   has_many :event_performance_categories
+  has_many :performances
 
   validates :name, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_114209) do
+ActiveRecord::Schema.define(version: 2021_01_09_132709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(version: 2021_01_09_114209) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "performances", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.bigint "performance_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["performance_category_id"], name: "index_performances_on_performance_category_id"
+    t.index ["user_id"], name: "index_performances_on_user_id"
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
@@ -60,5 +71,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_114209) do
 
   add_foreign_key "event_performance_categories", "event_categories"
   add_foreign_key "event_performance_categories", "performance_categories"
+  add_foreign_key "performances", "performance_categories"
+  add_foreign_key "performances", "users"
   add_foreign_key "roles", "users"
 end
