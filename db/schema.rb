a14a1cd3abbc30916b8683ca571364945106f497
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-12 16:56:16 -0300

    Add Genre model, test and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_162636) do
+ActiveRecord::Schema.define(version: 2021_03_12_195354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "show_categories", force: :cascade do |t|
     t.string "name"
