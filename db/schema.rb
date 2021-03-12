36fce7d1d92b6dc241e1cc8cc77b125b88846cb2
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-12 17:32:08 -0300

    Add model Show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_202859) do
+ActiveRecord::Schema.define(version: 2021_03_12_203125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 2021_03_12_202859) do
   create_table "show_categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "minimum_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/models/show_test.rb
+++ test/models/show_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class ReviewTest < ActiveSupport::TestCase
+class ShowTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-  # enderai
+  # end
 end
