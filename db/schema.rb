c0b13f6eef4706d70e48d881e35853c2affe75cd
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-12 17:00:24 -0300

    Add Faq model, test and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_195354) do
+ActiveRecord::Schema.define(version: 2021_03_12_195737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "faqs", force: :cascade do |t|
+    t.string "question"
+    t.string "answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
