62325675c94ffa2aaf768968b932cbcc8af54517
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-12 16:56:16 -0300

    Add Genre model, test and validations

---
+++ test/models/genre_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class GenreTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should not save genre without name" do
+    genre = Genre.new
+    assert_not genre.save, "Saved the genre without a title"
+  end
+end
