0fba7467b7e52001c53ff857dffb0eb4fd1e96f2
Nadia Salmen <nadiasalmen@gmail.com>
2021-03-14 23:35:02 -0300

    Changes in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :us, :home2 ]
+  skip_before_action :authenticate_user!, only: [ :home, :us ]
 
   def home
   end
