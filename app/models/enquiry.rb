64b602d8d03ef922ee2658c408a5ef4ed237dd48
nadiasalmen <nadiasalmen@gmail.com>
2021-06-09 21:48:16 -0300

    Add show ref to enquiries

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,5 +1,6 @@
 class Enquiry < ApplicationRecord
   belongs_to :user
-  
+  belongs_to :show
+
   validates :question, presence: true
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,6 @@
 class Show < ApplicationRecord
   belongs_to :user
   has_one :category
+  has_many :enquiries
   has_many :show_event_categories
 end
