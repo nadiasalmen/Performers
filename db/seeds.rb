fd634d8b0b7e2b2736cd7e5cdb1f7f53646d1b7a
nadiasalmen <nadiasalmen@gmail.com>
2021-06-13 22:53:00 -0300

    Add event_category model validations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+puts 'Cleaning database...'
+EventCategory.destroy_all
+
+
+puts 'Creating event categories'
+puts 'Creating event_category: Bar - Restó'
+event_category = EventCategory.new(
+  name: 'Bar - Restó',
+  description: 'Eventos públicos o privados en bares y restaurants'
+)
+event_category.image.attach(io: File.open('app/assets/images/ec_bar.png'), filename: "#{event_category.name}.png", content_type: 'image/png')
+event_category.save!
+puts 'Creating event_category: Social'
+event_category = EventCategory.new(
+  name: 'Social',
+  description: 'Eventos sociales públicos o privados'
+)
+event_category.image.attach(io: File.open('app/assets/images/ec_social.png'), filename: "#{event_category.name}.png", content_type: 'image/png')
+event_category.save!
+puts 'Creating event_category: Corporativo'
+event_category = EventCategory.new(
+  name: 'Corporativo',
+  description: 'Eventos corporativos privados'
+)
+event_category.image.attach(io: File.open('app/assets/images/ec_corporativo.png'), filename: "#{event_category.name}.png", content_type: 'image/png')
+event_category.save!
+puts 'Creating event_category: Evento Cultural'
+event_category = EventCategory.new(
+  name: 'Evento Cultural',
+  description: 'Eventos culturales'
+)
+event_category.image.attach(io: File.open('app/assets/images/ec_teatro.png'), filename: "#{event_category.name}.png", content_type: 'image/png')
+event_category.save!
+puts 'Creating event_category: Otros'
+event_category = EventCategory.new(
+  name: 'Otros',
+  description: 'Otros eventos'
+)
+event_category.image.attach(io: File.open('app/assets/images/ec_otros.png'), filename: "#{event_category.name}.png", content_type: 'image/png')
+event_category.save!
