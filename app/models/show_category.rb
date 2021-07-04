ce489130902468561ab204a72e4afbd1831b40d6
nadiasalmen <nadiasalmen@gmail.com>
2021-07-04 12:02:43 -0300

    Add relation has_many show_categories to show

--- app/models/show_category.rb
+++ app/models/show_category.rb
@@ -2,6 +2,9 @@ class ShowCategory < ApplicationRecord
   # active storage associations
   has_one_attached :image
 
+  #associations
+  belong_to :show
+
   # validations
   validates :name, inclusion: { in: ['Acústico', 'Magia', 'Banda', 'Infantil', 'Amenización', 'Baile', 'DJ', 'Animación', 'Stand Up', 'Otros'] }
   validates :name, presence: true
