643485c9862db9fb6165188585b32f45d865293b
nadiasalmen <nadiasalmen@gmail.com>
2021-07-04 00:10:55 -0300

    Add shows#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'index', to: 'shows#index'
   get 'coming_soon', to: 'pages#coming_soon'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/shows', to: 'shows#index'
+  get '/shows/:id', to: 'shows#show'
 end
