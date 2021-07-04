42f88b0bd77936bd4525a7e4ac40d67d4dac03ba
nadiasalmen <nadiasalmen@gmail.com>
2021-07-04 00:12:42 -0300

    Add shows#show action to ShowsController

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,7 +1,15 @@
 class ShowsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  before_action :set_show, only: [:show]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
   end
 
+  def show; end
+
+  private
+
+  def set_show
+    @show = Show.find(params[:id])
+  end
 end
